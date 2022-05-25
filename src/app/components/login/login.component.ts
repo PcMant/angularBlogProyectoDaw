5fc077023174d46a299cb4f5d0da322ed77f3161
Juan Molina <juan@pcmant.com>
2022-05-25 13:56:39 +0200

    update login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -20,15 +20,17 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this._loginService.login('juan77@','1234').subscribe(
+    this._loginService.login('juan','12345').subscribe(
       response => {
         if(response.result){
           this.credencials = response.result;
           console.log(this.credencials);
+          localStorage.setItem('sesion',JSON.stringify(this.credencials));
         }
         // console.log(response)
       },
       error => {
+        console.log('Usuario o la contraseña introduccidos son incorrectas.');
         console.log(`Este es el error: ${error}`);
       }
     );
