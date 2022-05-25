c317a99ad81ce7248ef036e88211370a2b1c9832
Juan Molina <juan@pcmant.com>
2022-05-25 18:12:36 +0200

    añadido libreria de iconos Bootstrap y terminado el Login, utiliza el localstrorage, queda aplicarlo a las distintas funcionalidades

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -16,18 +16,46 @@ export class LoginComponent implements OnInit {
 
   public credencials: any[] = [];
   public invalid: boolean = false;
+  public cuenta: any;
 
   constructor(
     private _loginService: LoginService,
     private _route: ActivatedRoute,
     private _router: Router,
     private fb: FormBuilder
-  ) { }
+  ) { 
+    this.cuenta = {
+      email: '',
+      password: ''
+    }
+  }
   
 
   ngOnInit(): void {
 
-    this._loginService.login('juan','12345').subscribe(
+    // this._loginService.login('juan','12345').subscribe(
+    //   response => {
+    //     if(response.result){
+    //       this.credencials = response.result;
+    //       console.log(this.credencials);
+    //       localStorage.setItem('sesion',JSON.stringify(this.credencials));
+    //     }
+    //     // console.log(response);
+    //     this.invalid = false;
+    //     this._router.navigate(['/home']);
+    //   },
+    //   error => {
+    //     console.log('Usuario o la contraseña introduccidos son incorrectas.');
+    //     console.log(`Este es el error: ${error}`);
+    //     this.invalid = true;
+    //   }
+    // );
+
+  }
+
+  onSubmit(): void{
+
+    this._loginService.login(this.cuenta.email,this.cuenta.password).subscribe(
       response => {
         if(response.result){
           this.credencials = response.result;
