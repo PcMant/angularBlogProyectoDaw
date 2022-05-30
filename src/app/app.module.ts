ec88493e735085aa81adde48659b0813d1ba37dd
Juan Molina <juan@pcmant.com>
2022-05-30 23:02:18 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,9 @@ import { PoliticaDePrivacidadComponent } from './components/politica-de-privacid
 import { AvisoLegalComponent } from './components/aviso-legal/aviso-legal.component';
 import { SearchComponent } from './components/search/search.component';
 import { LoginComponent } from './components/login/login.component';
+import { ArticleComponent } from './components/article/article.component';
+import { ArticleNewComponent } from './components/article-new/article-new.component';
+import { ArticleEditComponent } from './components/article-edit/article-edit.component';
 
 // Importación para poder usar formularios
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -29,9 +32,7 @@ import { ArticlesComponent } from './components/articles/articles.component';
 
 // Importando el modulo de moments
 import { MomentModule } from 'ngx-moment';
-import { ArticleComponent } from './components/article/article.component';
-import { ArticleNewComponent } from './components/article-new/article-new.component';
-import { ArticleEditComponent } from './components/article-edit/article-edit.component';
+
 
 @NgModule({
   declarations: [
--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// Esto necesita estar declarado para el uso de modales de bootstrap
+declare var window: any;
 
 @Component({
   selector: 'app-contacto',
@@ -8,8 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class ContactoComponent implements OnInit {
 
   public person: any;
+  formModal: any;
 
-  constructor() {
+  constructor(private _router: Router) {
     this.person = {
       nombre: '',
       email: '',
@@ -18,10 +22,43 @@ export class ContactoComponent implements OnInit {
    }
 
   ngOnInit(): void {
+
+    // Guadar identificador del modal de bootstrap en variable
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('winModal')
+    );
+  }
+
+  // Abrir modal de bootstrap
+  openFormModal(): void {
+    this.formModal.show();
+  }
+
+  // Cerrar modal de bootstrap
+  saveSomeThing(): void {
+    // confirm or save something
+    this.formModal.hide();
+  }
+
+  goHome(): void{
+    this._router.navigate(['/home']);
   }
 
   onSubmit(): void{
-    console.log(this.person);
+    // console.log(this.person);
+    if(
+      this.person.nombre != '' &&
+      this.person.nombre &&
+      this.person.nombre != null &&
+      this.person.email != '' &&
+      this.person.email &&
+      this.person.email != null &&
+      this.person.mensaje != '' &&
+      this.person.mensaje &&
+      this.person.mensaje != null
+    ){
+      this.openFormModal();
+    }
   }
 
 }
