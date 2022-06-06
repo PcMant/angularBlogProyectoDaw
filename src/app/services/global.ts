9e4a64f7d66eb6be6bf2b7b57323c4516a5cedcd
Juan Molina <juan@pcmant.com>
2022-06-07 01:12:16 +0200

    update comentarios

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
-// Fichero con configuraciones globales
+// Fichero con configuraciones globales, estos valores se modifican según las credenciales del API y su URL
 export var Global = {
     url: 'http://localhost/proyectoFinal_blog/PhpApiRestBlogProyectoDaw/',
     Authorization: 'AtWV3mYpVNrQvKdD4gWvjbF2imGLQh'
